91327c67dc4879c3fad84030005bfb48963806e6
gap <gap@intepi.net>
2023-07-21 21:38:04 +0200

    Update app.go

--- src/app.go
+++ src/app.go
@@ -202,7 +202,7 @@ type NvidiaSmiLog struct {
 			EnforcedPowerLimit string `xml:"enforced_power_limit"`
 			MinPowerLimit      string `xml:"min_power_limit"`
 			MaxPowerLimit      string `xml:"max_power_limit"`
-		} `xml:"power_readings"`
+		} `xml:"gpu_power_readings"`
 		Clocks struct {
 			GraphicsClock string `xml:"graphics_clock"`
 			SmClock       string `xml:"sm_clock"`
